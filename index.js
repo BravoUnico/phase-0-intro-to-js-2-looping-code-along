008c2467a46b52216fdc94a885283c03523a937e
Alejandro <sancha0701@gmail.com>
2022-07-18 21:28:18 -0400

    commit tree a5e062dd30958ac777adf1df9af225d9eec143b3

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+
+function writeCards(names, surprise) {  //  ✓ returns an array of thank you messages for each name provided to the function
+  let newCarts = []
+  for ( let i = 0; i < names.length ; i++ ) {
+    
+    newCarts.push(`Thank you, ${names[i]}, for the wonderful ${surprise} gift!`);
+  }
+  return newCarts
+}
+
+function countDown(integer) {
+  
+  while (integer > 0) {
+    console.log(integer--);
+  }
+  console.log(integer)
+}
+console.log(countDown(10))
